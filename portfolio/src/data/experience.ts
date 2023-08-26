70d3b8f8582091e16e1de431d645273070f58c93
isabellehuangg <huang.yuqing.issy@gmail.com>
2023-08-25 23:24:20 -0400

    Wording

--- portfolio/src/data/experience.ts
+++ portfolio/src/data/experience.ts
@@ -10,7 +10,7 @@ export const experience = [
         role: "Software Developer",
         company: "UW Midnight Sun",
         time: "Jun. 2023 - Present",
-        description: "Worked on data migration of Victini: a real-time communication system between the car and multiple microservices",
+        description: "Worked on migration of Victini: a real-time communication system between the car and multiple microservices",
         technologies: "Python, Docker"
     }
 ]
